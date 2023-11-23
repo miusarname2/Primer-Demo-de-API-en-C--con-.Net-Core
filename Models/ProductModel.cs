d776643fe6f5282ec14b264d116c0ea4c3c235fc
miusarname <oscarm.alvarezg@gmail.com>
2023-11-23 12:24:39 -0500

    feat: :sparkles: Se agregan unas acciones más al proyecto
    Se agregan especificamente las acciones de eliminar porducto y crear producto

---
+++ Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo1.Models
+{
+    public class ProductModel
+    {
+        public int? Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int? Stock { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using Demo1.Data;
+using Demo1.Models;
+
+namespace Demo1.Services
+{
+    public class ProductService
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductService(StoreDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void createProduct(ProductModel product)
+        {
+            ProductEntity productToAdd = new() 
+            { 
+                Name = product.Name,
+                Description = product.Description,
+                CreateDate = DateTime.Now,
+                Stock = product.Stock
+            };
+            _context.Products.Add(productToAdd);
+            _context.SaveChanges();
+        }
+
+        public void deleteProduct(int id) 
+        {
+            ProductEntity productToDelet = GetProduct(id);
+            _context.Products.Remove(productToDelet);
+        }
+
+        public ProductEntity GetProduct(int id) 
+        {
+            return _context.Products.Find(id);
+        }
+
+        public List<ProductEntity> GetProducts() 
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
